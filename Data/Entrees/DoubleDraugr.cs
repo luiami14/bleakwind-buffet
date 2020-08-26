c5b77ca2a6d2a70748c04992ffcfa57334183b2d
luiami14 <luiami14@F1093-25-2284>
2020-08-26 18:32:13 -0500

    Just finished the double draugr burger class

--- Data/Entrees/DoubleDraugr.cs
+++ Data/Entrees/DoubleDraugr.cs
@@ -12,5 +12,99 @@ namespace BleakwindBuffet.Data.Entrees
 {
     class Double_Draugr
     {
+        /// <summary>
+        /// Price of the burger
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.32;
+            }
+        }
+        /// <summary>
+        /// Gets the calories of the burger
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                return 843;
+            }
+        }
+        /// <summary>
+        /// a private variable that is set to true initially
+        /// </summary>
+        private bool ketchup = true;
+        /// <summary>
+        /// Checks if the user wants ketchup or not
+        /// </summary>
+        public bool Ketchup
+        {
+            get
+            {
+                return ketchup;
+            }
+            set
+            {
+                ketchup = value;
+            }
+        }
+        /// <summary>
+        /// Checks if the user wants the bun(gives the option)
+        /// </summary>
+        public bool Bun { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants mustard (gives the option)
+        /// </summary>
+        public bool Mustard { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants Pickles (gives the option)
+        /// </summary>
+        public bool Pickle { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants cheese (gives the option)
+        /// </summary>
+        public bool Cheese { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants tomato (gives the option)
+        /// </summary>
+        public bool Tomato { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants lettuce (gives the option)
+        /// </summary>
+        public bool Lettuce { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants mayo (gives the option)
+        /// </summary>
+        public bool Mayo { get; set; } = true;
+        /// <summary>
+        /// This method makes a new List everytime a user is odering this burger.
+        /// </summary>
+        public List<string> SpecialIntructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if (!Bun) instructions.Add("Hold bun");
+                if (!Ketchup) instructions.Add("Hold ketchup");
+                if (!Mustard) instructions.Add("Hold mustard");
+                if (!Pickle) instructions.Add("Hold pickle");
+                if (!Cheese) instructions.Add("Hold cheese");
+                if (!Tomato) instructions.Add("Hold tomato");
+                if (!Lettuce) instructions.Add("Hold lettuce");
+                if (!Mayo) instructions.Add("Hold mayo");
+                return instructions;
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the burger
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Double Draugr";
+        }
     }
+}
 }
