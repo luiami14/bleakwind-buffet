2b091e7f3b92966e908fd31c1a52491961929db0
luiami14 <luiami14@F1093-25-2284>
2020-08-26 14:36:21 -0500

    Started and finished with the SodaFlavor enum

---
+++ Data/Enums/SodaFlavor.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Author: Luis Amiel
+ * Class name: SodaFlavor.cs
+ * Purpose: Class used to represent the flavors of soda
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data.Enums
+{
+    /// <summary>
+    /// Provides soda flavors
+    /// </summary>
+    public enum SodaFlavor
+    {
+        Blackberry,
+        Cherry, 
+        Grapefruit, 
+        Lemon, 
+        Peach, 
+        Watermelon
+    }
+}
