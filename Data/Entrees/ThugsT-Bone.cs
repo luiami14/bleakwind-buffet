9af09c705c8166f95f87762f0287d454a08e23a5
luiami14 <luiami14@F1093-25-2284>
2020-08-26 20:03:39 -0500

    Just finished the thugs Tbone class.

--- Data/Entrees/ThugsT-Bone.cs
+++ Data/Entrees/ThugsT-Bone.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Author: Luis Amiel
- * Class name: Thugs T-Bone.cs
+ * Class name: ThugsTBone.cs
  * Purpose: Class used to represent one of the entrees in the menu
  */
 
@@ -10,7 +10,46 @@ using System.Text;
 
 namespace BleakwindBuffet.Data.Entrees
 {
-    class Thugs_T_Bone
+    public class ThugsTBone
     {
+        /// <summary>
+        /// Price of the T-Bone
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 6.44;
+            }
+        }
+        /// <summary>
+        /// Gets the calories of the T-Bone
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                return 982;
+            }
+        }
+        /// <summary>
+        /// This method makes a new List everytime a user is odering this T-Bone
+        /// </summary>
+        public List<string> SpecialIntructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                return instructions;
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the T-Bone
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Thugs T-Bone";
+        }
     }
 }
