e83529f1d85edb80e7b82db33426333e6f65cf3b
luiami14 <luiami14@F1093-04-2263>
2020-08-28 14:29:56 -0500

    Just finished the vokun salad class.

--- Data/Sides/VokunSalad.cs
+++ Data/Sides/VokunSalad.cs
@@ -1,10 +1,93 @@
-﻿using System;
+﻿/*
+ * Author: Luis Amiel
+ * Class name: VokunSalad.cs
+ * Purpose: Class used to represent one of the drinks in the menu
+ */
+
+using BleakwindBuffet.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Data.Sides
+namespace BleakwindBuffet.Data.Sides
 {
-    class VokunSalad
+    public class VokunSalad
     {
+        /// <summary>
+        /// private variable that initializes the size
+        /// </summary>
+        private Size size = Size.Small;
+        /// <summary>
+        /// Provides available sizes
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        /// <summary>
+        /// Price of the salad
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 0.93;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 1.28;
+                }
+                else if (size == Size.Large)
+                {
+                    return 1.82;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the calories of salad
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 41;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 52;
+                }
+                else if (size == Size.Large)
+                {
+                    return 73;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the salad
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return size.ToString() + "Vokun Salad";
+        }
     }
 }
