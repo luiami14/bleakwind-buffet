7b40421c4a9a67815a616530b33687340f544330
luiami14 <luiami14@F1093-04-2263>
2020-08-28 14:13:10 -0500

    Just finished the candlehearth coffee class

--- Data/Drinks/CandlehearthCoffee.cs
+++ Data/Drinks/CandlehearthCoffee.cs
@@ -1,10 +1,123 @@
-﻿using System;
+﻿/*
+ * Author: Luis Amiel
+ * Class name: CandlehearthCoffee.cs
+ * Purpose: Class used to represent one of the drinks in the menu
+ */
+
+using BleakwindBuffet.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Data.Drinks
 {
-    class CandlehearthCoffee
+    public class CandlehearthCoffee
     {
+        /// <summary>
+        /// private variable that initializes the size
+        /// </summary>
+        private Size size = Size.Small;
+        /// <summary>
+        /// Provides available sizes
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        /// <summary>
+        /// Price of the coffee
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 0.75;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 1.25;
+                }
+                else if (size == Size.Large)
+                {
+                    return 1.75;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the calories of coffee
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 7;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 10;
+                }
+                else if (size == Size.Large)
+                {
+                    return 20;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the drink
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return size.ToString() + "Candlehearth Coffee";
+        }
+        /// <summary>
+        /// This method makes a new List everytime a user is odering this drink
+        /// </summary>
+        public List<string> SpecialIntructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if (!Ice) instructions.Add("Add ice");
+                if (!RoomForCream) instructions.Add("Add cream");
+                return instructions;
+            }
+        }
+        /// <summary>
+        /// backing variable for the ice property
+        /// </summary>
+        private bool ice = true;
+        /// <summary>
+        /// Checks if the user wants the ice(gives the option)
+        /// </summary>
+        public bool Ice { get; set; } = true;
+        /// <summary>
+        /// backing variable for the cream property
+        /// </summary>
+        private bool roomforcream = true;
+        /// <summary>
+        /// chekcs if the user wants cream
+        /// </summary>
+        public bool RoomForCream { get; set; } = true;
+
     }
 }
