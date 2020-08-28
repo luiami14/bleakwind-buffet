a0cc56c520c59ee418dfa91818a165b879003f28
luiami14 <luiami14@F1093-23-2282>
2020-08-27 22:13:41 -0500

    Just started working on the sailor soda class in the drinks folder

--- Data/Drinks/SailorSoda.cs
+++ Data/Drinks/SailorSoda.cs
@@ -1,10 +1,109 @@
-﻿using System;
+﻿/*
+ * Author: Luis Amiel
+ * Class name: SailorSoda.cs
+ * Purpose: Class used to represent one of the drinks in the menu
+ */
+
+using BleakwindBuffet.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Data.Drinks
+namespace BleakwindBuffet.Data.Drinks
 {
-    class SailorSoda
+    public class SailorSoda
     {
+        /// <summary>
+        /// private variable that initializes the size
+        /// </summary>
+        private Size size = Size.Small;
+        /// <summary>
+        /// Provides available sizes
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        /// <summary>
+        /// Price of the Sailor soda
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                if(size == Size.Small)
+                {
+                    return 1.42;
+                }
+                else if(size== Size.Medium)
+                {
+                    return 1.74;
+                }
+                else if(size==Size.Large)
+                {
+                    return 2.07;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the calories of sailorSoda
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                if (small)
+                {
+                    return 117;
+                }
+                else if (medium)
+                {
+                    return 153;
+                }
+                else if (large)
+                {
+                    return 205;
+                }
+            }
+        }
+        /// <summary>
+        /// This method makes a new List everytime a user is odering this drink
+        /// </summary>
+        public List<string> SpecialIntructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if (!Ice) instructions.Add("Hold ice");
+                return instructions;
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the drink
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "sdsd";
+        }
+        /// <summary>
+        /// Checks if the user wants the ice(gives the option)
+        /// </summary>
+        public bool Ice { get; set; } = true;
+        /// <summary>
+        /// Checks if the user wants the flavor(gives the option)
+        /// </summary>
+        public bool Flavor { get; set; } = cherry;
     }
 }
