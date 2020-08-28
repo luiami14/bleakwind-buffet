10431a3dc4aa602c2a0248e77aaf0d56ca3879ce
luiami14 <luiami14@F1093-04-2263>
2020-08-28 14:42:54 -0500

    Just finished the Mad otar grits class.

--- Data/Sides/MadOtarGrits.cs
+++ Data/Sides/MadOtarGrits.cs
@@ -1,10 +1,93 @@
-﻿using System;
+﻿/*
+ * Author: Luis Amiel
+ * Class name: MadOtarGrits.cs
+ * Purpose: Class used to represent one of the sides in the menu
+ */
+
+using BleakwindBuffet.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Data.Sides
+namespace BleakwindBuffet.Data.Sides
 {
-    class MadOtarGrits
+    public class MadOtarGrits
     {
+        /// <summary>
+        /// private variable that initializes the size
+        /// </summary>
+        private Size size = Size.Small;
+        /// <summary>
+        /// Provides available sizes
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        /// <summary>
+        /// Price of the Grits
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 1.22;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 1.58;
+                }
+                else if (size == Size.Large)
+                {
+                    return 1.93;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the calories of Grits
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 105;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 142;
+                }
+                else if (size == Size.Large)
+                {
+                    return 179;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the Grits
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return size.ToString() + "Mad Otar Grits";
+        }
     }
 }
