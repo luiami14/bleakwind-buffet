9161aefa50e3b231e91353eba68ac0330fbaf817
luiami14 <luiami14@F1093-04-2263>
2020-08-28 13:43:44 -0500

    Just finished the markarth milk class

--- Data/Drinks/MarkarthMilk.cs
+++ Data/Drinks/MarkarthMilk.cs
@@ -1,10 +1,101 @@
-﻿using System;
+﻿/*
+ * Author: Luis Amiel
+ * Class name: MarkarthMilk.cs
+ * Purpose: Class used to represent one of the drinks in the menu
+ */
+
+using BleakwindBuffet.Data.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Data.Drinks
+namespace BleakwindBuffet.Data.Drinks
 {
-    class MarkarthMilk
+    public class MarkarthMilk
     {
+        /// <summary>
+        /// private variable that initializes the size
+        /// </summary>
+        private Size size = Size.Small;
+        /// <summary>
+        /// Provides available sizes
+        /// </summary>
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        /// <summary>
+        /// Price of the milk
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 1.42;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 1.74;
+                }
+                else if (size == Size.Large)
+                {
+                    return 2.07;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the calories of milk
+        /// </summary>
+        public int Calories
+        {
+            get
+            {
+                if (size == Size.Small)
+                {
+                    return 117;
+                }
+                else if (size == Size.Medium)
+                {
+                    return 153;
+                }
+                else if (size == Size.Large)
+                {
+                    return 205;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        /// <summary>
+        /// This method just prints out the name of the drink
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return size.ToString()+ "Markarth Milk";
+        }
+        /// <summary>
+        /// backing variable for the ice property
+        /// </summary>
+        private bool ice = true;
+        /// <summary>
+        /// Checks if the user wants the ice(gives the option)
+        /// </summary>
+        public bool Ice { get; set; } = true;
     }
 }
