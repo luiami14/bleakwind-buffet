e3e9b4757dc963c120a11d25d7142415321b6623
luiami14 <luiami14@F0064-09-2511>
2020-09-05 23:30:27 -0500

    Created the warriorwatertests!

---
+++ DataTests/UnitTests/DrinkTests/WarriorWaterTests.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Author: Zachery Brunner
+ * Class: WarriorWaterTests.cs
+ * Purpose: Test the MarkarthMilk.cs class in the Data library
+ */
+
+using Xunit;
+
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Drinks;
+
+namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
+{
+    public class WarriorWaterTests
+    {
+        [Fact]
+        public void ShouldNotIncludeIceByDefault()
+        {
+            WarriorWater wj = new WarriorWater();
+            Assert.False(wj.Ice);
+        }
+
+        [Fact]
+        public void ShouldByAbleToSetIce()
+        {
+            WarriorWater wj = new WarriorWater();
+            wj.Ice = true;
+            Assert.True(wj.Ice);
+            wj.Ice = false;
+            Assert.False(wj.Ice);
+        }
+
+    }
+}
