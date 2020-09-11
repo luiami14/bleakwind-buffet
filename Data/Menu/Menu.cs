20c9c63459804d596357d13329ffb60bfd7116f0
luiami14 <luiami14@E3071-10-0513>
2020-09-11 17:24:19 -0500

    Almost done with the menu class!

--- Data/Menu/Menu.cs
+++ Data/Menu/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using BleakwindBuffet.Data.Enums;
 using BleakwindBuffet.Data.Interface;
+using BleakwindBuffet.Data.Entrees;
 
 
 namespace BleakwindBuffet.Data.Menu
@@ -11,19 +12,32 @@ namespace BleakwindBuffet.Data.Menu
     {
         public static IEnumerable<IOrderItem> Entrees()
         {
-
+            List<IOrderItem> tj = new List<IOrderItem>();
+            tj.Add(new BriarheartBurger());
+            tj.Add(new DoubleDraugr());
+            tj.Add(new GardenOrcOmelette());
+            tj.Add(new PhillyPoacher());
+            tj.Add(new SmokehouseSkeleton());
+            tj.Add(new ThalmorTriple());
+            tj.Add(new ThugsTBone());
+            return tj;
         }
         public static IEnumerable<IOrderItem> Sides()
         {
+            List<IOrderItem> tj = new List<IOrderItem>();
 
         }
         public static IEnumerable<IOrderItem> Drinks()
         {
-
+            List<IOrderItem> tj = new List<IOrderItem>();
         }
         public static IEnumerable<IOrderItem> FullMenu()
         {
-
+            List<IOrderItem> tj = new List<IOrderItem>();
+            tj.AddRange(Entrees());
+            tj.AddRange(Drinks());
+            tj.AddRange(Sides());
+            return tj;
         }
     }
 }
