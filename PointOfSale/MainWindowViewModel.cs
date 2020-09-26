947856124e01cfec179f16def3975e34acc33f06
luiami14 <luiami14@E3071-12-0515>
2020-09-26 14:47:30 -0500

    Some progress

---
+++ PointOfSale/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MainWindowViewModel
+    {
+        public int SwitchView
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            SwitchView = 0;
+        }
+    }
+}
