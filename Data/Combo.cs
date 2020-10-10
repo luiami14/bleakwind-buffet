595eb2632e59b05a83f92bb6a3269d75f272db8f
luiami14 <luiami14@E3071-14-0517>
2020-10-10 01:08:23 -0500

    Progress

---
+++ Data/Combo.cs
@@ -0,0 +1,109 @@
+﻿using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Interface;
+using BleakwindBuffet.Data.Sides;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    public class Combo : IOrderItem, INotifyPropertyChanged
+    {
+        public double Price => throw new NotImplementedException();
+
+        public uint Calories => throw new NotImplementedException();
+
+        public List<string> SpecialInstructions => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        Drink ss = new Data.Drinks.SailorSoda();
+        Entree bb = new Data.Entrees.BriarheartBurger();
+        Side sd = new Data.Sides.DragonbornWaffleFries();
+        /// <summary>
+        /// 
+        /// </summary>
+        public Drink Drink
+        {
+            get
+            {
+                return ss;
+            }
+            set
+            {
+                ss = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Side Side
+        {
+            get
+            {
+                return sd;
+            }
+            set
+            {
+                sd = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Entree Entree
+        {
+            get
+            {
+                return bb;
+            }
+            set
+            {
+                bb = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void CollectionItemChangedListener(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Size")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else if(e.PropertyName == "Flavor")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else if(e.PropertyName == "SpecialInstructions")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Calories"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
+    }
+}
