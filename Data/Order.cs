b3930c599853caf21a0143b656dc1ac78c4d52f5
luiami14 <luiami14@E3071-03-0506>
2020-10-10 21:31:21 -0500

    Progress

--- Data/Order.cs
+++ Data/Order.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 
 namespace BleakwindBuffet.Data
@@ -18,31 +19,32 @@ namespace BleakwindBuffet.Data
             set
             {
                 salesTaxRate = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("SalesTaxRate"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Subtotal"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Total"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Tax"));
             }
         }
-        private double subTotal;
         public double Subtotal
         {
             get
             {
+                double subTotal = 0;
+                foreach(IOrderItem element in this)
+                {
+                    subTotal += element.Price;
+                }
                 return subTotal;
             }
         }
-        private double tax;
         public double Tax
         {
-            get
-            {
-                return tax;
-            }
+            get => Subtotal * salesTaxRate;
         }
-        private double total;
+ 
         public double Total
         {
-            get
-            {
-                return total;
-            }
+            get => Tax + Subtotal;
         }
         private static double nextOrderNumber = 1;
         private double number;
@@ -53,10 +55,55 @@ namespace BleakwindBuffet.Data
                 return number;
             }
         }
+        private uint calories;
+        public uint Calories
+        {
+            get
+            {
+                return calories;
+            }
+        }
         public Order()
         {
             number = nextOrderNumber;
             nextOrderNumber++;
+        }
+        void CollectionChangedListener(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Size")
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else if(e.PropertyName == "Subtotal")
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else if (e.PropertyName == "Tax")
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+            else if(e.PropertyName == "")
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
+            }
+        }
+        void ItemCollectionChangedListener(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Size")
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs("Subtotal"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Total"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Tax"));
+            }
         }
 
     }
