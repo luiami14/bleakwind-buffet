b7c8778efa7df0203c8cfa0580cc890d4f62ad29
luiami14 <luiami14@E3071-03-0506>
2020-10-10 22:29:44 -0500

    Progress

--- Data/Order.cs
+++ Data/Order.cs
@@ -1,7 +1,10 @@
-﻿using BleakwindBuffet.Data.Interface;
+﻿using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Interface;
+using BleakwindBuffet.Data.Sides;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Text;
 
@@ -68,31 +71,45 @@ namespace BleakwindBuffet.Data
             number = nextOrderNumber;
             nextOrderNumber++;
         }
-        void CollectionChangedListener(object sender, PropertyChangedEventArgs e)
+        void CollectionChangedListener(object sender, NotifyCollectionChangedEventArgs e)
         {
-            if (e.PropertyName == "Size")
+            OnPropertyChanged(new PropertyChangedEventArgs("Subtotal"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Total"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Tax"));
+
+            switch(e.Action)
             {
-                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
-                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
-                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
-            }
-            else if(e.PropertyName == "Subtotal")
-            {
-                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
-                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
-                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
-            }
-            else if (e.PropertyName == "Tax")
-            {
-                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
-                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
-                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
-            }
-            else if(e.PropertyName == "")
-            {
-                OnPropertyChanged(new PropertyChangedEventArgs("Price"));
-                OnPropertyChanged(new PropertyChangedEventArgs("Calories"));
-                OnPropertyChanged(new PropertyChangedEventArgs("SpecialInstructions"));
+                case NotifyCollectionChangedAction.Add:
+                    foreach(IOrderItem item in e.NewItems)
+                    {
+                        if(item is AretinoAppleJuice aj) { aj.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is CandlehearthCoffee cc) { cc.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is MarkarthMilk mm) { mm.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is SailorSoda ss) { ss.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is WarriorWater ww) { ww.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is DragonbornWaffleFries dj) { dj.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is FriedMiraak fm) { fm.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is MadOtarGrits mg) { mg.PropertyChanged += ItemCollectionChangedListener; }
+                        else if(item is VokunSalad vs) { vs.PropertyChanged += ItemCollectionChangedListener; }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach(IOrderItem item in e.OldItems)
+                    {
+                        if (item is AretinoAppleJuice aj) { aj.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is CandlehearthCoffee cc) { cc.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is MarkarthMilk mm) { mm.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is SailorSoda ss) { ss.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is WarriorWater ww) { ww.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is DragonbornWaffleFries dj) { dj.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is FriedMiraak fm) { fm.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is MadOtarGrits mg) { mg.PropertyChanged += ItemCollectionChangedListener; }
+                        else if (item is VokunSalad vs) { vs.PropertyChanged += ItemCollectionChangedListener; }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    throw new NotImplementedException(""Reset);
             }
         }
         void ItemCollectionChangedListener(object sender, PropertyChangedEventArgs e)
