94f568ffa30dad230355428b40568bd2785db18b
luiami14 <luiami14@E3071-03-0506>
2020-10-10 20:07:42 -0500

    Started the Order class.

---
+++ Data/Order.cs
@@ -0,0 +1,63 @@
+﻿using BleakwindBuffet.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    public class Order: ObservableCollection<IOrderItem>
+    {
+        private double salesTaxRate = 0.12;
+        public double SalesTaxRate
+        {
+            get
+            {
+                return salesTaxRate;
+            }
+            set
+            {
+                salesTaxRate = value;
+            }
+        }
+        private double subTotal;
+        public double Subtotal
+        {
+            get
+            {
+                return subTotal;
+            }
+        }
+        private double tax;
+        public double Tax
+        {
+            get
+            {
+                return tax;
+            }
+        }
+        private double total;
+        public double Total
+        {
+            get
+            {
+                return total;
+            }
+        }
+        private static double nextOrderNumber = 1;
+        private double number;
+        public double Number
+        {
+            get
+            {
+                return number;
+            }
+        }
+        public Order()
+        {
+            number = nextOrderNumber;
+            nextOrderNumber++;
+        }
+
+    }
+}
