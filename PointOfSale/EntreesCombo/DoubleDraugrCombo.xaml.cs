29099c6498d84a85eec59a2788bc6ff1d35c4437
luiami14 <luiami14@E3071-03-0506>
2020-10-18 15:15:35 -0500

    Created combo screens for entrees,sides,drinks

---
+++ PointOfSale/EntreesCombo/DoubleDraugrCombo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.EntreesCombo
+{
+    /// <summary>
+    /// Interaction logic for DoubleDraugrCombo.xaml
+    /// </summary>
+    public partial class DoubleDraugrCombo : UserControl
+    {
+        public DoubleDraugrCombo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PointOfSale/SidesCombo/VokunSaladCombo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.SidesCombo
+{
+    /// <summary>
+    /// Interaction logic for VokunSaladCombo.xaml
+    /// </summary>
+    public partial class VokunSaladCombo : UserControl
+    {
+        public VokunSaladCombo()
+        {
+            InitializeComponent();
+        }
+    }
+}
