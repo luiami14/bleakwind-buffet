56e0e1cd7b99a9ceceeabe18c4215eb1b6849caf
Luis Amiel <luiami14@ksu.edu>
2020-11-08 22:18:57 -0600

    Done

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -15,15 +15,25 @@ namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// This is  read only ILogger
+        /// </summary>
         private readonly ILogger<IndexModel> _logger;
-
+        /// <summary>
+        /// This property mentions the MenuItems
+        /// </summary>
         public IEnumerable<IOrderItem> MenuItems { get; set; }
-
+        /// <summary>
+        /// This is the indexmodel
+        /// </summary>
+        /// <param name="logger"></param>
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// This is the onGet method that displays all of the backend code
+        /// </summary>
         public void OnGet()
         {
             MenuItems = Menu.Search(SearchTerms);
@@ -31,16 +41,34 @@ namespace Website.Pages
             MenuItems = Menu.FilterByCalories(MenuItems, CaloriesMin, CaloriesMax);
             MenuItems = Menu.FilterByPrice(MenuItems, PriceMin, PriceMax);
         }
+        /// <summary>
+        /// This property searchTerms
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
+        /// <summary>
+        /// The property Category
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public string[] Category { get; set; }
+        /// <summary>
+        /// The property CaloriesMin
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public int CaloriesMin { get; set; }
+        /// <summary>
+        /// The property CaloriesMax
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public int CaloriesMax { get; set; } = 1000;
+        /// <summary>
+        /// The property PriceMin
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public double PriceMin { get; set; }
+        /// <summary>
+        /// The property PriceMax
+        /// </summary>
         [BindProperty(SupportsGet = true)]
         public double PriceMax { get; set; } = 10;
     }
